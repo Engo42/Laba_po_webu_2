ff75520f427286c50d39f8658cdddaf500d853db
Engo42 <73175817+Engo42@users.noreply.github.com>
2021-04-30 21:31:22 +1000

    Add files via upload

--- KeyHandlers.js
+++ KeyHandlers.js
@@ -75,8 +75,6 @@ function touchMoveHandler(e) {
 	var touch = e.targetTouches[0];
 	target_x = (touch.pageX - canvas.offsetLeft)*scaleX;
     target_y = (touch.pageY - canvas.offsetTop)*scaleY;
-	ctx.fillText("Xmov " + touch.pageX, 5, canvas.height - 85);
-	ctx.fillText("Ymov " + touch.pageY, 5, canvas.height - 65);
 }
 document.addEventListener("touchstart", touchMoveHandler, false);
 document.addEventListener("touchend", touchEndHandler, false);
